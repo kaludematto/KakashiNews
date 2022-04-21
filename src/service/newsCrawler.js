829abed6a207c6a2b4637c60f9d9e2a72fcf7d76
kaludematto <kalu.dematto@gmail.com>
2022-04-21 16:22:05 -0300

    add crawler

---
+++ src/service/newsCrawler.js
@@ -0,0 +1,20 @@
+const Crawler = require('crawler');
+
+const c = new Crawler({
+    encode: null,
+    // This will be called for each crawled page
+    callback: (error, res, done) => {
+        if (error) {
+            console.log(error);
+        } else {
+            const $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($('title').text());
+        }
+        done();
+    }
+});
+
+const anime = c.queue('https://www.intoxianime.com/')
+console.log(anime);
